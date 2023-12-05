138dd912a4d13a1e337b8a53af55ae5a928c6a1c
RokoVrdo <106666710+RokoVrdo@users.noreply.github.com>
2023-12-05 11:11:55 +0100

    Add files via upload

--- endless.py
+++ endless.py
@@ -18,15 +18,17 @@ lives=3
 class Pirat():
     def __init__(self):
         # self.pirat_image=pygame.image.load("Slike\\likpirata.png").convert_alpha()
-        self.pirat_image=pygame.transform.scale(pygame.image.load("Slike\\likpirata.png"), (160, 160)).convert_alpha()
+        self.pirat_image=pygame.transform.scale(pygame.image.load("Slike\\likpirata.png"), (120, 120)).convert_alpha()
         self.pirat_rect=self.pirat_image.get_rect(midbottom= (300, 650))
         
 
         self.kretanje_desno=False
         self.kretanje_lijevo=False
+        self.jump=False
 
-        self.brzina= 3
-        self.gravity= 0
+        self.brzina= 2
+        self.gravity= 5
+        self.jump_height= 15
 
     def kretanje_komande(self):
         keys = pygame.key.get_pressed()
@@ -34,8 +36,9 @@ class Pirat():
             self.kretanje_desno = True
         if keys[pygame.K_LEFT]:
             self.kretanje_lijevo = True
+        # if event.type == pygame.KEYDOWN: 
         if keys[pygame.K_SPACE]:
-            self.gravity=-30
+            self.jump = True
     
     def kretanje_ekran(self):
         if self.kretanje_desno and self.pirat_rect.right < 650:
@@ -44,9 +47,14 @@ class Pirat():
         if self.kretanje_lijevo and self.pirat_rect.left > -50:
             self.pirat_rect.x -= self.brzina
             self.kretanje_lijevo=False
-        if  self.pirat_rect.bottom>=650:
-            self.gravity += 1
-            self.pirat_rect.y += self.gravity
+        if  self.jump:
+            self.pirat_rect.y -= self.gravity
+            self.gravity -= 2                 
+            if self.gravity < -self.jump_height:
+                self.jump=False
+                self.gravity=self.jump_height
+
+                screen.blit(self.pirat_image, self.pirat_rect)
 		    
             if self.pirat_rect.bottom >= 650:
                 self.pirat_rect.bottom = 650
@@ -63,7 +71,7 @@ class Novcic(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.novcic_slika=pygame.image.load("Slike\\slikacoin.png").convert_alpha()
-        self.novcic_rect=self.novcic_slika.get_rect(midtop= (randint(40, 710), -50))
+        self.novcic_rect=self.novcic_slika.get_rect(midtop= (randint(150, 450), -50))
 
         self.brzina_dropa=1
 
@@ -89,26 +97,39 @@ def drop_novcici():
     if len(objekti) == 0:
         novi_novcic = Novcic()
         objekti.add(novi_novcic)
-    if len(objekti) == 1:
+
+    if score>=25:
+        if len(objekti) == 1:
+            for objekt in objekti.sprites():
+                if objekt.novcic_rect.bottom > 300:
+                    novi_novcic = Novcic()
+                    objekti.add(novi_novcic)
+    if score>=50:
         for objekt in objekti.sprites():
-            if objekt.novcic_rect.bottom > 300:
-                novi_novcic = Novcic()
-                objekti.add(novi_novcic)
-    if len(objekti) == 2:
+            objekt.brzina_dropa=1.5
+    
+    if score>=75:
+        if len(objekti) == 2:
+            for objekt in objekti.sprites():
+                if objekt.novcic_rect.bottom > 550:
+                    novi_novcic = Novcic()
+                    objekti.add(novi_novcic)
+    
+    if score>=100:
         for objekt in objekti.sprites():
-            if objekt.novcic_rect.bottom > 550:
-                novi_novcic = Novcic()
-                objekti.add(novi_novcic)
-    # if len(objekti) == 3:
-    #     for objekt in objekti.sprites():
-    #         if objekt.novcic_rect.bottom > 450:
-    #             novi_novcic = Novcic()
-    #             objekti.add(novi_novcic)
+            objekt.brzina_dropa=2
+        
+    if score>=150:
+        if len(objekti) == 3:
+            for objekt in objekti.sprites():
+                if objekt.novcic_rect.bottom > 450:
+                    novi_novcic = Novcic()
+                    objekti.add(novi_novcic)
 
 
     
     for objekt in objekti.sprites():
-        if objekt.novcic_rect.bottom > 750:
+        if objekt.novcic_rect.bottom > 660:
             objekti.remove(objekt)
     
 
@@ -143,7 +164,7 @@ while True:
                 aktivnost_igrice=True
                 objekti.remove(novcic)
             else:
-                if novcic.novcic_rect.bottom > 750:
+                if novcic.novcic_rect.bottom > 660:
                     lives-=1
                     if lives==0:
                         aktivnost_igrice=False
