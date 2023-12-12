88701bbfcab42754b88165c471363d5dcda6288f
MisterMario5 <106661036+MisterMario5@users.noreply.github.com>
2023-12-12 16:09:20 +0100

    Update Scroll_bar_primjer.py
    Poboljšana verzija sound scrolla koja omogućuje i stišavanje sound efekeata uz već prethodnu muziku

--- Igrica/Scroll_bar_primjer.py
+++ Igrica/Scroll_bar_primjer.py
@@ -85,16 +85,19 @@ pygame.display.set_caption("Login")
 menu_surface = pygame.image.load("Slike/Pozadina_menu.png").convert()
 test_font = pygame.font.Font(None, 50) #Moram pronaći neki dobar Font/ napraviti
 naslov_font = pygame.font.Font(None, 150)
+podnaslov_font = pygame.font.Font(None, 100)
 
 
 #S ostalima se dogovoriti za Logo
 zvuk = 0.5
+zvuk2 = 0.5
 bg_music = pygame.mixer.Sound("Audio/He's a Pirate.mp3")
 bg_music.set_volume(zvuk)
 bg_music.play(loops= -1)
-jump_sound  = pygame.mixer.Sound("Audio/Bonk Sound Effect.mp3")
-jump_sound.set_volume(0.5)
+click_sound  = pygame.mixer.Sound("Audio/Bonk Sound Effect.mp3")
+click_sound.set_volume(zvuk2)
 stisnut = 0
+stisnut2 = 0
 
 
 def main_menu(): #Iz ovoga dalje biramo druge "prozore". Moram napraviti animaciju i uskladiti dizajn s njom.
@@ -135,7 +138,10 @@ def main_menu(): #Iz ovoga dalje biramo druge "prozore". Moram napraviti animaci
 def options(): #Moram nadodati za smanjivanje muzike i zvukova + možda i objašnjenje za kontrole/radnju
     while True:
         global zvuk
-        global stisnut 
+        global stisnut
+        global zvuk2
+        global stisnut2
+
         options_surface = pygame.image.load("Slike/Background_options.png").convert()
         screen.blit(options_surface, (0,0))
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
@@ -143,15 +149,33 @@ def options(): #Moram nadodati za smanjivanje muzike i zvukova + možda i objaš
         
 
         opcije_tekst = naslov_font.render("Opcije" ,False, "White")
-        opcije_rect = opcije_tekst.get_rect(center = (300, 100))
+        opcije_rect = opcije_tekst.get_rect(center = (300, 60))
         screen.blit(opcije_tekst, opcije_rect)
+
+
+        muzika_tekst = podnaslov_font.render("Muzika" ,False, "Gray")
+        muzika_rect = muzika_tekst.get_rect(center = (200, 230))
+        screen.blit(muzika_tekst, muzika_rect)
 
         racun = round(zvuk,2)*100
         zvuk_tekst = test_font.render(f"{round(racun)}%",False, "Black")
-        zvuk_rect = zvuk_tekst.get_rect(center = (500, 400))
+        zvuk_rect = zvuk_tekst.get_rect(center = (500, 300))
         pygame.draw.rect(screen, "White", zvuk_rect)
         pygame.draw.rect(screen, "Brown", zvuk_rect,2)
         screen.blit(zvuk_tekst, zvuk_rect)
+
+
+        efekt_tekst = podnaslov_font.render("Zvučni efekti" ,False, "Gray")
+        efekt_rect = efekt_tekst.get_rect(center = (270, 430))
+        screen.blit(efekt_tekst, efekt_rect)
+
+
+        racun2 = round(zvuk2,2)*100
+        zvuk2_tekst = test_font.render(f"{round(racun2)}%",False, "Black")
+        zvuk2_rect = zvuk2_tekst.get_rect(center = (500, 500))
+        pygame.draw.rect(screen, "White", zvuk2_rect)
+        pygame.draw.rect(screen, "Brown", zvuk2_rect,2)
+        screen.blit(zvuk2_tekst, zvuk2_rect)
 
         OPTIONS_BACK = Gumb(pygame.image.load("Slike/Pozadina_gumb5.png").convert(), (100, 700), "Vrati se", test_font, "White", "Green")
 
@@ -160,28 +184,51 @@ def options(): #Moram nadodati za smanjivanje muzike i zvukova + možda i objaš
 
 
         #"(self, image_line, pos_line, image_button, pos_button)"
-        OPTIONS_SOUND = Scroll(pygame.image.load("Slike/Scol_linija1.png").convert(), (100, 400), pygame.image.load("Slike/Scrol_gumb.png").convert())
+        OPTIONS_SOUND = Scroll(pygame.image.load("Slike/Scol_linija1.png").convert(), (100, 300), pygame.image.load("Slike/Scrol_gumb.png").convert())
         
         OPTIONS_SOUND.update(screen)
 
+        OPTIONS_SOUND2 = Scroll(pygame.image.load("Slike/Scol_linija1.png").convert(), (100, 500), pygame.image.load("Slike/Scrol_gumb.png").convert())
+        
+        OPTIONS_SOUND2.update(screen)
+
         if stisnut == 0:
-            OPTIONS_MUSIC = Gumb(pygame.image.load("Slike/Unmute.png").convert(), (50, 400), "", test_font, "White", "Green")
+            OPTIONS_MUSIC = Gumb(pygame.image.load("Slike/Unmute.png").convert(), (50, 300), "", test_font, "White", "Green")
             OPTIONS_MUSIC.changeColor(OPTIONS_MOUSE_POS)
             OPTIONS_MUSIC.update(screen)
         else:
-            OPTIONS_MUSIC = Gumb(pygame.image.load("Slike/Mute.png").convert(), (50, 400), "", test_font, "White", "Green")
+            OPTIONS_MUSIC = Gumb(pygame.image.load("Slike/Mute.png").convert(), (50, 300), "", test_font, "White", "Green")
             OPTIONS_MUSIC.changeColor(OPTIONS_MOUSE_POS)
             OPTIONS_MUSIC.update(screen)
+
+        if stisnut2 == 0:
+            OPTIONS_MUSIC2 = Gumb(pygame.image.load("Slike/Unmute.png").convert(), (50, 500), "", test_font, "White", "Green")
+            OPTIONS_MUSIC2.changeColor(OPTIONS_MOUSE_POS)
+            OPTIONS_MUSIC2.update(screen)
+        else:
+            OPTIONS_MUSIC2 = Gumb(pygame.image.load("Slike/Mute.png").convert(), (50, 500), "", test_font, "White", "Green")
+            OPTIONS_MUSIC2.changeColor(OPTIONS_MOUSE_POS)
+            OPTIONS_MUSIC2.update(screen)
 
 
         if OPTIONS_SOUND.checkForInput1(OPTIONS_MOUSE_POS):  
             if OPTIONS_SOUND.changeButtonPosition(OPTIONS_MOUSE_POS, screen, zvuk):
                 zvuk = (OPTIONS_MOUSE_POS[0]- 110)/280 #Promijeni kad postaviš na drugo mjesto
                 #print(zvuk)
-                print(racun)
+                #print(racun)
                 bg_music.set_volume(zvuk)
                 pygame.mixer.unpause()
                 stisnut = 0
+
+        if OPTIONS_SOUND2.checkForInput1(OPTIONS_MOUSE_POS):  
+            if OPTIONS_SOUND2.changeButtonPosition(OPTIONS_MOUSE_POS, screen, zvuk):
+                zvuk2 = (OPTIONS_MOUSE_POS[0]- 110)/280 #Promijeni kad postaviš na drugo mjesto
+                #print(zvuk)
+                print(racun2)
+                click_sound.set_volume(zvuk2)
+                stisnut2 = 0
+
+
 
 
 
@@ -191,7 +238,7 @@ def options(): #Moram nadodati za smanjivanje muzike i zvukova + možda i objaš
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    jump_sound.play()
+                    click_sound.play()
                     main_menu()
                 if OPTIONS_MUSIC.checkForInput(OPTIONS_MOUSE_POS):
                     if zvuk == 0:
@@ -205,32 +252,20 @@ def options(): #Moram nadodati za smanjivanje muzike i zvukova + možda i objaš
                         pygame.mixer.pause()
                         #print("2")
                         stisnut = 1
+                if OPTIONS_MUSIC2.checkForInput(OPTIONS_MOUSE_POS):
+                    if zvuk2 == 0:
+                        zvuk2 = 0.5
+                        click_sound.set_volume(zvuk2)
+                        print(zvuk2)
+                        stisnut2 = 0
+                    else:
+                        zvuk2 = 0
+                        click_sound.set_volume(zvuk2)
+                        print(zvuk2)
+                        stisnut2 = 1
 
         pygame.display.update()
 
-    while True:
-        ACHIVMENT_MOUSE_POS = pygame.mouse.get_pos()
-
-        screen.fill("Black")
-
-        achivment_tekst = test_font.render("Ovdje su achivmenti",False, "White")
-        achivment_rect = achivment_tekst.get_rect(center = (300, 300))
-        screen.blit(achivment_tekst, achivment_rect)
-
-        ACHIVMENT_BACK = Gumb(pygame.image.load("Slike/Pozadina_gumb2.png").convert(), (300, 500),"Vrati se", test_font, "White", "Green")
-
-        ACHIVMENT_BACK.changeColor(ACHIVMENT_MOUSE_POS)
-        ACHIVMENT_BACK.update(screen)
-
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if ACHIVMENT_BACK.checkForInput(ACHIVMENT_MOUSE_POS):
-                    main_menu()
-        pygame.display.update()
 
 
 options()
