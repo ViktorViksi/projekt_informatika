40f16296af155b8988423921735a8f260db19e1d
MisterMario5 <106661036+MisterMario5@users.noreply.github.com>
2023-12-13 18:01:50 +0100

    Update THE CATCHER.py
    Scroll funkcija pravilno implementirana

--- The Catcher/THE CATCHER.py
+++ The Catcher/THE CATCHER.py
@@ -146,6 +146,27 @@ class Scroll():
         self.y_pos_button = self.y_pos_line
         self.rect_line = self.image_line.get_rect(midleft = (self.x_pos_line, self.y_pos_line))
         self.rect_button = self.image_button.get_rect(center = (self.x_pos_button, self.y_pos_button))
+        
+
+
+    def update(self,screen):
+        screen.blit(self.image_line, self.rect_line)
+
+    def checkForInput1(self,position):
+        if position[0] in range(self.rect_line.left +10, self.rect_line.right-9) and position[1] in range(self.rect_line.top, self.rect_line.bottom):
+            return True
+        return False
+
+
+    def changeButtonPosition(self,position, screen, zvuk):
+        self.jump_sound  = pygame.mixer.Sound("Audio/Bonk Sound Effect.mp3")
+        self.jump_sound.set_volume(0.5)
+        self.x_pos_button = int(position[0])
+        self.rect_button = self.image_button.get_rect(center = (self.x_pos_button, self.y_pos_button))
+        screen.blit(self.image_button, self.rect_button)
+        if pygame.mouse.get_pressed()[0]:
+            print(position)
+            return True
 
 
 def pause_options(scrn, oldScreen):
